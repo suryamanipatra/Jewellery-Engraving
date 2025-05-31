3be43536c54c729224634baa8186456f18e7823f
suryamanipatra <suryamanipatra114@gmail.com>
2025-05-31 15:39:41 +0530

    bug fix and some minior modification etc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 5173,   
     proxy: {
       '/api': {
-        target: 'http://backend:5000', 
+        target: 'http://localhost:5000', 
         changeOrigin: true,
         secure: false,
       },
@@ -21,7 +21,7 @@ export default defineConfig({
   // server: {
   //   proxy: {
   //     '/api': {
-  //       target: 'http://192.168.0.110:8000', 
+  //       target: 'http://localhost:8000', 
   //       changeOrigin: true,
   //       secure: false,
   //     },
